3255199e6be83161878fd592b6c37cf6503984e5
madara1319 <mateusz.bik@op.pl>
2024-04-23 21:57:00 +0200

    still worikng on entrance module

--- src/index.js
+++ src/index.js
@@ -42,12 +42,15 @@ class View{
     this.optionsDisplay=document.querySelector(".options");
     this.enterBox=document.querySelector(".entering");
 
-    this.baseFuncButton.addEventListener('click',this.toggleElement.bind(this,this.optionsDisplay));
-    this.enterProbesButton.addEventListener('click',this.toggleElement.bind(this,this.enterBox));
     
+    //toggle do do przyciskow wprowadzania 
+    this.baseFuncButton.addEventListener('click',(event)=>this.toggleElement(this.optionsDisplay,this.enterBox,event));  
+    this.enterProbesButton.addEventListener('click',(event)=>this.toggleElement(this.enterBox,this.optionsDisplay,event));  
+    
+
     this.selectedOption=document.querySelector(".selection");
     this.selectedOption.addEventListener('change',this.handleOptionChange.bind(this));
-    //this.selectedOption.addEventListener('change',this.handleUserChange.bind(this));
+
     //do slidera
     this.amplitudeSlider=document.querySelector(".amplitudeSlider");
     this.frequencySlider=document.querySelector(".frequencySlider");
@@ -60,19 +63,25 @@ class View{
     document.addEventListener('DOMContentLoaded',this.setupCharts.bind(this));
   }
   //wyswietlanie ukrywanie opcji wprowadzania danych
-  toggleElement(element, event){
+  toggleElement(firstElement,secondElement, event){
     event.preventDefault();
-    console.log(element);
+    //console.log(firstElement);
 
-    const isOpen=element.classList.contains("open");
+    const isFirstOpen=firstElement.classList.contains("open");
+    const isSecondOpen=secondElement.classList.contains("open");
 
-    if(isOpen){
-      element.classList.add("hidden");
-      element.classList.remove("open");
+    if(isFirstOpen){
+      firstElement.classList.add("hidden");
+      firstElement.classList.remove("open");
+      
     }
     else{
-      element.classList.remove("hidden");
-      element.classList.add("open");
+      if(isSecondOpen){
+        secondElement.classList.remove("open");
+        secondElement.classList.add("hidden");
+      }
+      firstElement.classList.remove("hidden");
+      firstElement.classList.add("open");
     }
   }
 //cos nie tak 
@@ -92,8 +101,19 @@ class View{
   }
   //need to handle multiple sliders events
   handleSlider(event){
-    const amplitudeValue=event.target.value;
-    const frequencyValue=event.target.value;
+
+    let amplitudeValue;
+    let frequencyValue;
+    console.log('event to ' + event);
+    console.log('id to ' + event.target.id);
+    if(event.target.id==='amplitudeSlider'){
+      amplitudeValue=event.target.value;
+      frequencyValue=this.frequencySlider.value;
+    }
+    else if(event.target.id==='frequencySlider'){
+      amplitudeValue=this.amplitudeSlider.value;
+      frequencyValue=event.target.value;
+    }
     const selectedOption=this.selectedOption.value;
     console.log("amplituda wynosi: " + amplitudeValue + " czestotliwosc wynosi: " + frequencyValue + " wybrana funkcja to " + selectedOption);
     this.drawChart(selectedOption, amplitudeValue, frequencyValue);
@@ -129,10 +149,10 @@ class View{
     }
   }
 //rysuj wykres
-  drawChart(optionValue, amplitudeValue, frequencyValue, customData=[]){
-    if(this.sampleChart){
-      this.sampleChart.destroy();
-    }
+  drawChart(optionValue, amplitudeValue = 1, frequencyValue = 10, customData=[]){
+   // if(this.sampleChart){
+   //   this.sampleChart.destroy();
+   // }
     let labels=[];
     let data=[];
 
@@ -143,10 +163,13 @@ class View{
     }
     else{
     switch (optionValue){
+
       case "Sine function": 
+
       data=SignalGenerator.generateSineWave(amplitudeValue,frequencyValue);
       labels=Array.from({length:10},(_,i)=>i.toString());
       break;
+
       case "Quadratic function":
       
       data=SignalGenerator.generateSquareWave(4,amplitudeValue,frequencyValue);
@@ -157,53 +180,50 @@ class View{
       data=SignalGenerator.generateTriangleWave(4,amplitudeValue,frequencyValue);
       labels=Array.from({length:10},(_,i)=>i.toString());
       break;
+
         default:
         break;
       }
     }
+    
 
     //nie wiem czy one na siebie nie nachodza
-//    if(this.sampleChart){
-//      this.sampleChart.destroy();
-//    }
-
-    const chartType=customData.length > 0 ? 'bar' : 'line';
-
-    const myChart=new Chart(this.sampleChart,{
-      type:chartType,
-      data:{
-        labels:labels,
-        datasets:[{
-          label:"Custom",
-          data:data,
-          fill:false,
-
-        backgroundColor:'rgba(255,99,132,0.5)',
-          borderColor:'rgb(255,99,132,1)',
-          tension:0.1
-        }]
-      },
-      options:{
-        responsive:true,
-        maintainAspectRatio:false,
-        scales:{
-          x:{
-            title:{
-              display:true,
-              text:'X'
-            }
-          },
-          y:{
-            title:{
-              display:true,
-              text:'Y'
-            }
+    if(this.sampleChart){
+      this.sampleChart.destroy();
+    }
+  this.sampleChart = new Chart(document.getElementById('sampleChart').getContext('2d'), {
+    type: customData.length > 0 ? 'bar' : 'line',
+    data: {
+      labels,
+      datasets: [{
+        label: 'Waveform',
+        data,
+        fill: false,
+        backgroundColor: 'rgba(255,99,132,0.5)',
+        borderColor: 'rgb(255,99,132,1)',
+        tension: 0.1
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: 'X'
+          }
+        },
+        y: {
+          title: {
+            display: true,
+            text: 'Y'
           }
         }
       }
-    });
-  }
-
+    }
+  });
+}
 }
 
 function main(){
