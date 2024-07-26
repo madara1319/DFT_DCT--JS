bcfd8679cc1fb2f69d7731572e4237f12afee25c
madara1319 <mateusz.bik@op.pl>
2024-07-26 21:03:15 +0200

    minor change

---
+++ src/ReverseTransformation.js
@@ -0,0 +1,10 @@
+class ReverseTransformation{
+  constructor(signal){
+    this.signal=signal;
+  }
+
+  reverseTranform(){
+    throw new Error('Method "reverseTransform" must be be implemented')
+  }
+}
+export {ReverseTransformation}
